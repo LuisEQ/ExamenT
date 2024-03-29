7a1152b4c6f9c80ced89d9226394799e8a47b0f6
LuisEQ <luiseduardoql@hotmail.com>
2024-03-29 01:56:46 -0700

    Added searchbar

--- components/ui/SearchBar.js
+++ components/ui/SearchBar.js
@@ -1,10 +1,51 @@
-import { View, StyleSheet, TextInput } from "react-native"
-function SearchBar(){
-    return (
-        <View></View>
-    )
+import { useState } from "react";
+import { View, StyleSheet, TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+function SearchBar({onChanged}) {
+  const [searchBarText, setSearchBarText] = useState("");
+
+  function searchBarHandler(text) {
+    setSearchBarText(text);
+    onChanged();
+  }
+  return (
+    <View style={styles.searchSection}>
+        <View style={styles.iconContainer}>
+            
+      <Ionicons name="search" size={24} color={"#777c7c"} />
+        </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          keyboardType={"default"}
+          maxLength={150}
+          autoCapitalize={"none"}
+          autoCorrect={true}
+          onChangeText={searchBarHandler}
+          value={searchBarText}
+          placeholder={"Buscar"}
+          secureTextEntry={false}
+        />
+      </View>
+    </View>
+  );
 }
 
 export default SearchBar;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  searchSection: {
+    flexDirection: "row",
+    backgroundColor: "#ccc",
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginHorizontal: 4,
+  },
+  iconContainer:{
+    marginHorizontal: 4,
+  },
+  inputContainer:{
+    padding: 4,
+  }
+});
--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,21 +1,33 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import IconButton from "../components/ui/buttons/IconButton";
 import PacientItem from "../components/pacientInfo/PacientItem";
 import Title from "../components/ui/Title";
+import SearchBar from "../components/ui/SearchBar";
 
-function ListScreen({onNewItem}) {
-    function addNewItemHandler(){
-        {onNewItem()}
+function ListScreen({ onNewItem }) {
+    function searchBarHandler(){
+
     }
-    return (
-        <View>
-            <Title>Registro </Title>
-            <PacientItem/>
-            <IconButton onPress={addNewItemHandler}/>
-        </View>
-    )
-}
+  function addNewItemHandler() {
+    {
+      onNewItem();
+    }
     
+  }
+  return (
+    <View>
+      <Title>Registro </Title>
+      <SearchBar onChanged={searchBarHandler}/>
+      <View>
+        <PacientItem />
+        <PacientItem />
+        <PacientItem />
+      </View>
+      <IconButton onPress={addNewItemHandler} />
+    </View>
+  );
+}
+
 export default ListScreen;
 
 const styles = StyleSheet.create({});
